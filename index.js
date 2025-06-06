23c834e1844ae1bcbc63fef0a5f01121ee88bd5d
Denilson-Oasis <denilsonschefer@gmail.com>
2025-06-05 21:06:04 -0300

    Neutraliza index.js da raiz

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.post('/webhook', (req, res) => {
-  console.log('Recebido webhook do Jotform:', req.body);
-
-  // Aqui você pode adicionar o código para enviar a mensagem via WhatsApp
-
-  res.status(200).send('Webhook recebido com sucesso');
-});
-
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
+// Arquivo desativado
